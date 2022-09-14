971022406471de09773f559e84cea604e4de3fae
dbswldhks00 <90027370+dbswldhks00@users.noreply.github.com>
2022-09-15 02:04:38 +0900

    player

---
+++ My project/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 MoveVecter = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMove();
+    }
+    void UpdateMove()
+    {
+
+    }
+    public void ProcessInput(Vector3 moveDirection)
+    {
+    
+    }
+}
